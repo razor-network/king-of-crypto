edd69c9ba16afa3bda14b61e267b3445d3f4ec23
Hrishikesh Huilgolkar <hrishikesh911@gmail.com>
2019-11-30 16:32:52 +0700

    init

---
+++ contracts/King.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.5.10;
+
+import "../../contracts/contracts/IDelegator.sol";
+
+
+contract King {
+    IDelegator public delegator;
+    delegator = IDelegator(_delegatorAddress);
+    
+    function addFeed(uint256 jobId) {
+
+    }
+
+    function findKing() {
+        delegator.
+    }
+
+}
